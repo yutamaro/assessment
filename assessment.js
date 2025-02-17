3af4f7a27a97a59828d7cf242808c0a26b46256e
yutamaro <125805709+yutamaro@users.noreply.github.com>
2025-02-17 13:55:24 +0900

    CSS　の改善

--- assessment.js
+++ assessment.js
@@ -14,15 +14,30 @@ assessmentButton.addEventListener(
     }
 
     // 診断結果表示エリアの作成
- resultDivision.innerText = '';
-  const heading = document.createElement('h3');
-  heading.innerText = '診断結果';
-  resultDivision.appendChild(heading);
+    resultDivision.innertText = '';
+    
+    //headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
 
-  const paragraph = document.createElement('p');
-  const result = assessment(userName);
-  paragraph.innerText = result;
-  resultDivision.appendChild(paragraph);
+    //bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
+
+    const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
+    const result = assessment(userName);
+    paragraph.innerText = result;
+    bodyDivision.appendChild(paragraph);
+
+    //resultDivision にBootstrap のスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    //headerDivision と bodyDivision をresultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
+
 
     //ツイートエリアの作成
     tweetDivision.innerText = '';
